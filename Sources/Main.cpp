b328a210001a23248e525ed4097b165386a87f96
Aman Menda <aman.menda@epitech.eu>
2024-04-20 22:07:28 +0100

    feat: frame test imgui

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,8 +1,13 @@
+#include"imgui.h"
+#include"imgui_impl_glfw.h"
+#include"imgui_impl_opengl3.h"
+
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
 #define ERROR_EXIT_CODE -1
+#define GLSL_VERSION "#version 330"
 
 int main(void)
 {
@@ -24,16 +29,39 @@ int main(void)
 	gladLoadGL();
 
 	glViewport(0, 0, 800, 800);
-	
-	// back buffer color - navy, front to default and swap them
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(GLSL_VERSION);
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+
+		ImGui::Begin("Test");
+		ImGui::Text("This is the ImGui part !");
+		ImGui::End();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
