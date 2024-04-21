83e2fad88686efc8c2576e6e04bb46d84076f2bd
Aman Menda <92232342+AmanMenda@users.noreply.github.com>
2024-04-21 13:55:18 +0100

    Update emscripten_mainloop_stub.h

--- Libraries/emscripten/emscripten_mainloop_stub.h
+++ Libraries/emscripten/emscripten_mainloop_stub.h
@@ -8,22 +8,6 @@
 //     to craft easy-to-past repro when they want to discuss features or report issues.
 //   - It conveys at a glance that this is a no-BS framework, it won't take your main loop away from you.
 //   - It is generally nice and elegant.
-// - However, comes Emscripten... it is a wonderful and magical tech but it requires a "main loop" function.
-// - Only some of our examples would run on Emscripten. Typically the ones rendering with GL or WGPU ones.
-// - I tried to refactor those examples but felt it was problematic that other examples didn't follow the
-//   same layout. Why would the SDL+GL example be structured one way and the SGL+DX11 be structured differently?
-//   Especially as we are trying hard to convey that using a Dear ImGui backend in an *existing application*
-//   should requires only a few dozens lines of code, and this should be consistent and symmetrical for all backends.
-// - So the next logical step was to refactor all examples to follow that layout of using a "main loop" function.
-//   This worked, but it made us lose all the nice things we had...
-
-// Since only about 3 examples really need to run with Emscripten, here's our solution:
-// - Use some weird macros and capturing lambda to turn a loop in main() into a function.
-// - Hide all that crap in this file so it doesn't make our examples unusually ugly.
-//   As a stance and principle of Dear ImGui development we don't use C++ headers and we don't
-//   want to suggest to the newcomer that we would ever use C++ headers as this would affect
-//   the initial judgment of many of our target audience.
-// - Technique is based on this idea: https://github.com/ocornut/imgui/pull/2492/
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <functional>
